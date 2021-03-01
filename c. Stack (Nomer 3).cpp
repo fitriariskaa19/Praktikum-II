9de14252abbbf1a138090f2638078bb6ce774753
fitriariskaa19 <72713210+fitriariskaa19@users.noreply.github.com>
2021-03-01 22:35:36 +0700

    Add files via upload

---
+++ c. Stack (Nomer 3).cpp
@@ -0,0 +1,18 @@
+/* Pembalikan Kalimat */
+/* Fitri Ariska */
+/* 20051397082 */
+/* 2020B */
+
+#include<iostream>
+#include<string.h>
+
+
+using namespace std;
+int main(){
+ char kalimat[100];
+ cout<<"Masukan Kalimat :"; cin.getline (kalimat , sizeof (kalimat) );
+ int x = strlen(kalimat);
+ for(int i=x-1;i>=0;i--){
+  cout<<kalimat[i];
+ }
+}
